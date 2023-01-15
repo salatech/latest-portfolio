65cf79aab488ad316fdd61aa260c6595de080442
sala-design <solahudeenbabatunde@gmail.com>
2023-01-15 15:22:44 +0100

    firebase

---
+++ src/config/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvClJsR8EODlYuTNFleCOmQAFTtfrbyAY",
+  authDomain: "portfolio-76757.firebaseapp.com",
+  projectId: "portfolio-76757",
+  storageBucket: "portfolio-76757.appspot.com",
+  messagingSenderId: "118418067036",
+  appId: "1:118418067036:web:8241d6e29a28926905a099"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
