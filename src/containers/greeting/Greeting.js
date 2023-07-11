9a6b369ea56ba66f44a30b9570bc6cc7cd68b1ee
sala-design <solahudeenbabatunde@gmail.com>
2023-07-11 19:04:46 +0100

    pic

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -4,7 +4,7 @@ import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
 import { greeting } from "../../portfolio";
 import { Fade } from "react-reveal";
-import Me from "../../assests/images/me.png"
+import Me from "../../assests/images/mee.png"
 
 export default function Greeting(props) {
   const theme = props.theme;
