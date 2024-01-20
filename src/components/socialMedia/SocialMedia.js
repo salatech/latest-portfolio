3b4214a1683b6feaf16415dde5acaa72286b284e
salatech <solahudeenbabatunde@gmail.com>
2024-01-20 15:21:34 +0100

    ..

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./SocialMedia.css";
 import { socialMediaLinks } from "../../portfolio";
 import styled from "styled-components";
@@ -13,27 +13,63 @@ const IconWrapper = styled.span`
 	}
 `;
 
-export default function socialMedia(props) {
+const DraggableSocialMedia = () => {
+	useEffect(() => {
+		const socialMediaDiv = document.querySelector(".social-media-div");
+		let offsetX, offsetY;
+
+		const handleDragStart = (e) => {
+			offsetX = e.clientX - socialMediaDiv.getBoundingClientRect().left;
+			offsetY = e.clientY - socialMediaDiv.getBoundingClientRect().top;
+			socialMediaDiv.style.position = "absolute";
+			socialMediaDiv.style.pointerEvents = "none";
+
+			document.addEventListener("mousemove", handleDragMove);
+			document.addEventListener("mouseup", handleDragEnd);
+		};
+
+		const handleDragMove = (e) => {
+			const x = e.clientX - offsetX;
+			const y = e.clientY - offsetY;
+			socialMediaDiv.style.left = `${x}px`;
+			socialMediaDiv.style.top = `${y}px`;
+		};
+
+		const handleDragEnd = () => {
+			document.removeEventListener("mousemove", handleDragMove);
+			document.removeEventListener("mouseup", handleDragEnd);
+			socialMediaDiv.style.pointerEvents = "auto";
+		};
+
+		socialMediaDiv.addEventListener("mousedown", handleDragStart);
+
+		return () => {
+			socialMediaDiv.removeEventListener("mousedown", handleDragStart);
+		};
+	}, []);
+
 	return (
-		<div className="social-media-div">
+		<div
+			className="social-media-div"
+			title="Drag me!"
+		>
 			{socialMediaLinks.map((media) => {
 				return (
 					<a
+						key={media.id} // Don't forget to add a unique key
 						href={media.link}
 						className={`icon-button`}
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						<IconWrapper
-							{...media}
-							{...props}
-						>
+						<IconWrapper {...media}>
 							<i className={`fab ${media.fontAwesomeIcon}`}></i>
 						</IconWrapper>
-						{/* <span></span> */}
 					</a>
 				);
 			})}
 		</div>
 	);
-}
+};
+
+export default DraggableSocialMedia;
