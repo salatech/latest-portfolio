1ac5409cbfefb73d18a82e3de57a6f1f93a47b70
salatech <solahudeenbabatunde@gmail.com>
2024-01-20 14:45:27 +0100

    fix: degraded react version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom";
 import { BaseProvider, LightTheme } from "baseui";
 import { Provider as StyletronProvider } from "styletron-react";
 import { Client as Styletron } from "styletron-engine-atomic";
@@ -11,16 +11,14 @@ import * as serviceWorker from "./serviceWorker";
 import "./assests/font-awesome/css/all.css";
 
 const engine = new Styletron();
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-  <StyletronProvider value={engine}>
-    <BaseProvider theme={LightTheme}>
-      <App />
-    </BaseProvider>
-  </StyletronProvider>
+ReactDOM.render(
+	<StyletronProvider value={engine}>
+		<BaseProvider theme={LightTheme}>
+			<App />
+		</BaseProvider>
+	</StyletronProvider>,
+	document.getElementById("root")
 );
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
